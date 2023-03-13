1e815f9ffe8e0d3da802961995199d93b88dc6c6
CHI-HAO, CHUANG <xd0931021@gmail.com>
2023-03-13 21:39:18 +0800

    Auto Commit

--- fontend/src/index.js
+++ fontend/src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import $ from 'jquery'
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
+import LoginBox from './login';
 import TopBar from './top-bar';
+import SideBar from './side-bar';
+
+import './index.css';
+import './fonts/font.css'
+import './fonts/material-symbols.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-var need_loaded = 1;
+var need_loaded = 0;
 var userSid, userName;
+
+const year = new Date().getFullYear()
+
+function CopyRight() {
+    return (
+        <div id="copyright">
+            Copyright © {year} <a href="https://github.com/AloneAlongLife/ASL-System" target="_blank" rel="noreferrer">莊智皓</a>
+        </div>
+    )
+}
 
 class MainContent extends React.Component {
     constructor(props) {
@@ -16,19 +31,26 @@ class MainContent extends React.Component {
         this.state = {
             userSid: userSid,
             userName: userName,
-            open: false
+            menuOpen: false
         };
     }
 
+    clickMenu() {
+        this.setState((state) => {
+            return { menuOpen: !state.menuOpen }
+        });
+    }
+
     render() {
-        console.log(123)
         return (
             <React.StrictMode>
                 <TopBar
+                    clickMenu={this.clickMenu.bind(this)}
+                    menuOpen={this.state.menuOpen}
                     name={this.state.userName}
-                    menuOpen={this.state.open}
-                    userOpen={false}
                 />
+                <SideBar open={this.state.menuOpen} />
+                <CopyRight />
             </React.StrictMode>
         );
     }
@@ -42,17 +64,29 @@ function render() {
         root.render(
             <MainContent />
         );
-        reportWebVitals();
+        // reportWebVitals();
     }
 }
 
-$.getJSON(
-    `/api/info/user/current`,
-    (response) => {
+function renderLogin() {
+    root.render(
+        <LoginBox school="嘉義市嘉華中學" />
+    );
+}
+
+$.getJSON({
+    url: `/api/info/user/current`,
+    success: (response) => {
         userSid = response.data.sid;
         userName = response.data.name;
-        need_loaded--;
+        render();
+    },
+    error: (response) => {
+        if (response.status === 403) {
+            renderLogin()
+        }
+        else {
+            setTimeout(window.location.reload, 1000);
+        }
     }
-);
-
-render();
+});
--- fontend/src/top-bar.js
+++ fontend/src/top-bar.js
@@ -8,13 +8,18 @@ export default class TopBar extends React.Component {
 
         this.state = {
             clickMenu: props.clickMenu,
-            clickUserMenu: props.clickUserMenu,
             showPage: props.showPage,
+            userOpen: false,
             name: props.name,
-            icon: props.icon,
         }
-
     }
+
+    clickUserMenu() {
+        this.setState((state) => {
+            return { userOpen: !state.userOpen }
+        });
+    }
+
     logout() {
         $.ajax({
             url: "/api/logout/current",
@@ -24,20 +29,19 @@ export default class TopBar extends React.Component {
 
     render() {
         const menuOpen = this.props.menuOpen;
-        const userOpen = this.props.userOpen;
         return (
             <div id="top-bar">
                 <div
                     className={`menu ms ${menuOpen ? "open" : ""}`}
                     onClick={this.state.clickMenu}
                 />
-                <img src="/static/img/logo.png" alt="Logo" />
+                <img src="/static/img/logo.png" alt="Logo" onClick={() => { this.state.showPage(0) }} />
                 <div className="empty" />
-                <div className="user-tag" onClick={this.state.clickUserMenu}>
-                    <img src={this.state.icon} alt="User Logo" />
+                <div className="user-tag" onClick={this.clickUserMenu.bind(this)}>
+                    <img src="/api/info/user/current/icon" alt="User Logo" />
                     <div>{this.state.name}</div>
                 </div>
-                <div className={`user-menu ${userOpen ? "open" : ""}`}>
+                <div className={`user-menu ${this.state.userOpen ? "open" : ""}`}>
                     <div className="user-menu-tag" onClick={() => { this.state.showPage(1) }}>
                         <p className="ms">account_circle</p>
                         <p>帳號</p>
